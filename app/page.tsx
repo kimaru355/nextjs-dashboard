4b7ca345fea7f42feb1ff9a871fe3b955cddcac7
kimaru355 <emmanuelkimaru355@gmail.com>
2024-02-27 00:40:58 +0300

    images

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -30,6 +31,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Hero Image"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={350}
+            height={800}
+            className="block md:hidden"
+            alt="Hero Image"
+          />
         </div>
       </div>
     </main>
